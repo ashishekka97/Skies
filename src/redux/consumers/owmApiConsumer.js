b27d5d05e1bf275578d4b809eef30e0b4326d973
Ashish Ekka <ashish.ekka@synapse.com>
2019-10-04 11:27:52 +0530

    Add Image Assets

--- src/redux/consumers/owmApiConsumer.js
+++ src/redux/consumers/owmApiConsumer.js
@@ -1,5 +1,5 @@
 import NetworkManager from '../utils/networkManager';
-import { apikey as key } from '../../config/apiKey';
+import { apikey as key } from '../../../config/apiKey';
 
 export function fetchWeatherByCityApi(city = 'Nagpur') {
   return NetworkManager.get('/data/2.5/weather', {q: city, APPID: key}, {})
